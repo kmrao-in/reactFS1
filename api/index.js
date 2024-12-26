c14fd30faf627b8129674a4b13abb9b0e0110b23
YMahesh Rao K <kmrao_in@yahoo.com>
2024-12-27 01:06:28 +0530

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+ mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
 const app = express();
 app.listen(3000, () => {
   console.log('Server listening on port 3000 ');
 });
+
+// app.get('/', (req, res) =>{
+//     res.json({
+//         message: 'API is owkring',
+//     });
+// });
